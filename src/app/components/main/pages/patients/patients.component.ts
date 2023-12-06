f3daa09c10480cbd695b5db5dffbbfbeba959b7d
giezz <giezz@vk.com>
2023-12-07 00:02:21 +0300

    Тест запроса.

--- src/app/components/main/pages/patients/patients.component.ts
+++ src/app/components/main/pages/patients/patients.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import {tuiIconSearchLarge} from "@taiga-ui/icons";
 import {IPatient} from "../../../../models/IPatient";
 import {patients} from "../../../../data/patients";
+import {PatientService} from "./patient.service";
 
 @Component({
   selector: 'app-patients',
@@ -9,9 +10,23 @@ import {patients} from "../../../../data/patients";
   styleUrls: ['./patients.component.css']
 })
 export class PatientsComponent {
+
+  constructor(
+    private patientService: PatientService
+  ) {
+  }
+
   protected readonly tuiIconSearchLarge = tuiIconSearchLarge;
 
   patients: readonly IPatient[] = patients;
+
+  click() {
+    this.patientService.getAll().subscribe(
+      data => {
+        console.log(data)
+      }
+    )
+  }
 
   readonly columns: string[] = ['lastName', 'firstName', 'middleName', 'gender', 'birthDate', 'deathDate', 'status']
 
