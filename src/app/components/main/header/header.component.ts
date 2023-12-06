2ff0169d96da17e75db91f1b7aa60e5cda6a71d3
giezz <giezz@vk.com>
2023-12-06 23:59:31 +0300

    Выход из аккаунта.

--- src/app/components/main/header/header.component.ts
+++ src/app/components/main/header/header.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {AuthService} from "../../auth/auth.service";
+import {Router} from "@angular/router";
 
 @Component({
   selector: 'app-header',
@@ -10,4 +12,14 @@ export class HeaderComponent {
   accountName: string = 'Иванов Иван Иванович'
   avatarUrl: string = 'https://sun59-2.userapi.com/s/v1/ig2/epMn0ETCPwtF6sGc5NAZk4stywWGnTLwmWTsucugRgsZQL12RyQRx-AfMPVZh3Bgo2LTVNMOB3a41K49Hnwavy84.jpg?size=50x50&quality=95&crop=89,675,456,456&ava=1'
 
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) { }
+
+  logout() {
+    this.authService.logout()
+    this.router.navigate(['/login']).then()
+  }
 }
