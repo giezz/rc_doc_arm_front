85f9d84e707596daa5301439d9013f9c89047428
giezz <giezz@vk.com>
2023-12-09 03:00:38 +0300

    Добавление валидации.

--- src/app/components/auth/login/auth.component.ts
+++ src/app/components/auth/login/auth.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit} from '@angular/core';
-import {FormControl, FormGroup} from "@angular/forms";
+import {FormControl, FormGroup, Validators} from "@angular/forms";
 import {Router} from "@angular/router";
 import {AuthService} from "../auth.service";
 import {AuthRequest} from "../auth-request";
@@ -28,8 +28,8 @@ export class AuthComponent implements OnInit {
   }
 
   loginForm = new FormGroup({
-    username: new FormControl(''),
-    password: new FormControl(''),
+    username: new FormControl('', Validators.required),
+    password: new FormControl('', Validators.required),
   });
 
   submit() {
