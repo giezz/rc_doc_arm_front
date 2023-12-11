717fd919502cccc36d75f5e5d54da37e304a0f43
giezz <giezz@vk.com>
2023-12-11 23:52:59 +0300

    Мои пациенты.

---
+++ src/app/components/main/pages/my-patients/my-patients-list/patient-detail-card/patient-detail-card.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-patient-detail-card',
+  templateUrl: './patient-detail-card.component.html',
+  styleUrls: ['./patient-detail-card.component.css']
+})
+export class PatientDetailCardComponent {
+
+}
