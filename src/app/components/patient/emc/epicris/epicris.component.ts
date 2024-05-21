9ff49ba60986ae4ed897d42bcdfca52cad67e201
giezz <giezz@vk.com>
2024-05-22 01:39:03 +0300

    Добавлен компонент просмотра выписного эпикриза.

---
+++ src/app/components/patient/emc/epicris/epicris.component.ts
@@ -0,0 +1,33 @@
+import {Component, inject, Input, OnDestroy, OnInit} from '@angular/core';
+import {PatientService} from "../../../../services/patient.service";
+import {Observable} from "rxjs";
+import {Epicris} from "../../../../models/epicris";
+import {ActivatedRoute} from "@angular/router";
+import {HospitalizationHistory} from "../../../../models/HospitalizationHistory";
+
+@Component({
+  selector: 'app-epicris',
+  templateUrl: './epicris.component.html',
+  styleUrls: ['./epicris.component.css']
+})
+export class EpicrisComponent implements OnInit, OnDestroy {
+
+    @Input()
+    hospHistory: HospitalizationHistory;
+
+    private patientService: PatientService = inject(PatientService);
+    private activeRoute: ActivatedRoute = inject(ActivatedRoute);
+
+    patientCode: number
+
+    epicrisises$ = new Observable<Epicris[]>();
+
+    ngOnInit(): void {
+        this.patientCode = Number(this.activeRoute.parent?.snapshot.params['patientCode']);
+        this.epicrisises$ = this.patientService.getEpicrisises(this.patientCode, this.hospHistory.id)
+    }
+
+    ngOnDestroy(): void {
+    }
+
+}
