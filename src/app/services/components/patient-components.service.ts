a63459394f1f4f956d8957575b33ed68ab8c1d74
giezz <giezz@vk.com>
2024-05-22 01:41:44 +0300

    Добавлено кеширование историй госпитализаций.

--- src/app/services/components/patient-components.service.ts
+++ src/app/services/components/patient-components.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core';
 import {ReplaySubject} from "rxjs";
 import {Patient} from "../../models/patient";
+import {HospitalizationHistory} from "../../models/HospitalizationHistory";
 
 @Injectable({
     providedIn: 'root'
@@ -10,7 +11,14 @@ export class PatientComponentsService {
     private patientSubject = new ReplaySubject<Patient | null>(1);
     public patient$ = this.patientSubject.asObservable();
 
-    public setPatient(patient: Patient) {
+    public setPatient(patient: Patient | null) {
         this.patientSubject.next(patient)
+    }
+
+    private hospHistorySubject = new ReplaySubject<HospitalizationHistory[] | null>(1)
+    public hospHistory$ = this.hospHistorySubject.asObservable();
+
+    public setHospHistory(hospHistory: HospitalizationHistory[] | null) {
+        this.hospHistorySubject.next(hospHistory);
     }
 }
